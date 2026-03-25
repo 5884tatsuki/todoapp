e849003b3a5538525194bcba4a751ff311d611e3
5884tatsuki <basketman.tatuki.2004@gmail.com>
2026-03-25 15:35:22 +0900

    テスト コミット

---
+++ src/page.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function Page() {
+  return (
+    <div className="page">
+      <h1>Welcome to My Page</h1>
+      <p>This is a simple page using React and Vite.</p>
+    </div>
+  )
+}
+
+export default Page
